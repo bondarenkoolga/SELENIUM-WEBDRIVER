76302a057d134f53e7b51210bb209276556cc874
bondarenkoolga <bondarenkoolga@gmail.com>
2020-11-24 14:13:32 +0600

    Update CheckLink.java

--- src/test/java/com/gmail/CheckLink.java
+++ src/test/java/com/gmail/CheckLink.java
@@ -14,10 +14,12 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+
 public class CheckLink {
     @Test
     public void First_Test() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+       // WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.get("http://localhost/litecart/public_html/admin/?app=countries&doc=countries");
         WebDriverWait wait = new WebDriverWait(driver, 20/*seconds*/);
         driver.findElement(By.name("username")).sendKeys("admin");
@@ -25,6 +27,7 @@ public class CheckLink {
         driver.findElement(By.name("login")).click();
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("[name='countries_form']"))));
         driver.findElement(By.cssSelector("[title='Edit']")).click();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//h1[contains(text(), 'Edit Country')]"))));
 
         String existingWindow = driver.getWindowHandle();
@@ -42,55 +45,9 @@ public class CheckLink {
             driver.close();
             driver.switchTo().window(existingWindow);
         }
-    }
-       // choiseFromListAndClick(By.cssSelector("tr.row td[style]"), driver);
-      //  clickAndOpenAllWindows(By.cssSelector("i.fa-external-link"), driver, wait);
-       // driver.quit();
-
-    private void choiseFromListAndClick(By locator, WebDriver driver){
-
-    }
-    private void clickAndOpenAllWindows (By locator, WebDriver driver, WebDriverWait wait) {
-        String mainWindow = driver.getWindowHandle();
-        Set<String> oldWindows = driver.getWindowHandles();
-        List<WebElement> list = driver.findElements(locator);
-        for (WebElement we:list) {
-            we.click();
-            openAndCloseNewWindow(oldWindows, driver, wait);
-            driver.switchTo().window(mainWindow);
-        }
-    }
-
-    private void openAndCloseNewWindow(Set<String> oldWindows, WebDriver driver, WebDriverWait wait ) {
-        implicitlyWaitOff(driver);
-        String newWindow = wait.until(anyWindowOtherThan(oldWindows));
-        driver.switchTo().window(newWindow);
-        String title = driver.getTitle();
-        if (title.contains("Wikipedia"))
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1")));
-        else
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button#acct_btn")));
-        implicitlyWaitOn(driver);
         driver.close();
     }
-    private void implicitlyWaitOn(WebDriver driver) {
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-    }
-
-    private void implicitlyWaitOff(WebDriver driver) {
-        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    }
 
 
 
-    public ExpectedCondition<String> anyWindowOtherThan (final Set<String> oldWindows) {
-        return new ExpectedCondition<String>() {
-            @Override
-            public String apply(WebDriver d) {
-                Set<String> handles = d.getWindowHandles();
-                handles.removeAll(oldWindows);
-                return  handles.size()>0 ? handles.iterator().next() : null;
-            }
-        };
-    }
 }
