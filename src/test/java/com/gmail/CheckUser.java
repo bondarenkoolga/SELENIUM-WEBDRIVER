4a529a5fe68b78e42bda85d1c8c1139a22a3cbbc
bondarenkoolga <bondarenkoolga@gmail.com>
2020-12-03 00:10:37 +0600

    Update CheckUser.java

--- src/test/java/com/gmail/CheckUser.java
+++ src/test/java/com/gmail/CheckUser.java
@@ -6,17 +6,18 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
 ;import java.nio.charset.Charset;
 import java.util.Random;
 
 public class CheckUser {
     @Test
-    public void First_Test() throws InterruptedException {
-         WebDriver driver = new ChromeDriver();
-         driver.get("http://localhost/litecart/public_html/en/");
-
+        public void First_Test() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost/litecart/public_html/en/");
         String email = GenerateLogin()+"@gmail.com";
         String password = "1234567";
         driver.findElement(By.cssSelector("form[name='login_form'] table tr:last-child")).click();
@@ -29,21 +30,22 @@ public class CheckUser {
         driver.findElement(By.name("address2")).sendKeys("-");
         driver.findElement(By.name("postcode")).sendKeys("12345");
         driver.findElement(By.name("city")).sendKeys("Alabama");
-        Select country = new Select(driver.findElement(By.name("country_code")));
-        country.selectByVisibleText("United States");
-        Select zone = new Select(driver.findElement(By.cssSelector("select[name='zone_code']")));
-        zone.selectByVisibleText("Alabama");
+      //  Select country = new Select(driver.findElement(By.name("country_code")));
+     //   country.selectByVisibleText("United States");
+        driver.findElement(By.name("country_code")).sendKeys("United States");
         driver.findElement(By.name("email")).sendKeys(email);
         driver.findElement(By.name("phone")).sendKeys("+18452245869");
         driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.name("confirmed_password")).sendKeys(password);
+
+        driver.findElement(By.name("country_code")).sendKeys(password);
         driver.findElement(By.name("create_account")).click();
-        //выход из аккаунта
         driver.findElement(By.cssSelector("div#box-account div.content li:last-child a")).click();
         Thread.sleep(1000);
         login(email,password, driver);
         Thread.sleep(1000);
         driver.findElement(By.cssSelector("div#box-account div.content li:last-child a")).click();
+        driver.quit();
     }
     public String GenerateLogin() {
         char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
@@ -54,13 +56,11 @@ public class CheckUser {
             sb.append(c);
         }
         String output = sb.toString();
-     //   System.out.println(output);
         return output;
     }
-        private void login(String email, String password, WebDriver driver) {
-            driver.findElement(By.name("email")).sendKeys(email);
-            driver.findElement(By.name("password")).sendKeys(password);
-            driver.findElement(By.name("login")).click();
-        }
-
+    private void login(String email, String password, WebDriver driver) {
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("login")).click();
+    }
 }
